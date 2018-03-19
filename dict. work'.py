d01fc685377b37c64810a67c433e3511c5ce2837
Roshna Magar <roshna.magar@stu.jefferson.kyschools.us>
2018-03-19 08:15:09 -0400

    dict. work

---
+++ dict. work'.py
@@ -0,0 +1,7 @@
+dict_roshnagurung =
+ "gratitude": the quality of being thankful; readiness to show appreciation for and toreturn kindness",
+"rhetoric":the art of effective or persuasive speaking or writing, especially the use of figures of speech and other compositional techniques",
+"agnostic";a person who believes that nothing is known or can be known of the existence or nature of God or of anything beyond material phenomena; a person who claims neither
+faith nor disbelief in God",
+"misogynist":a person who dislikes, despises, or is strongly prejudiced against women",
+

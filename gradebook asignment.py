d16aa1ac29d5da382a7adf05e2be4f8011ad057e
Roshna Magar <roshna.magar@stu.jefferson.kyschools.us>
2018-03-28 08:16:52 -0400

    gradebook asignment by roshna magar

---
+++ gradebook asignment.py
@@ -0,0 +1,34 @@
+Gradebook Average Assignment
+
+grade_1 = input("what grade do you have in this class 1 eglish?:")
+grade_2 = input("what grade do you have in this class 2 chemistry?:")
+grade_3 = input("what grade do you have in this class 3 visual art?:")
+grade_4 =input("what grade do you have in this class 4 computational thinking?:")
+grade_5 =input("what grade do you have in this class 5 math?:")
+ 
+all_grade = [garde_1,grade_2,grade_3,grade_4,grade_5]
+print:grade_list.
+
+print:grade_list.
+
+garde_average = (eglish+chemistry+visual art+computation thinking+math)
+
+all_grade.append(ag)
+
+if grade_average < 60:
+    print "your average garde is a u"
+if average_grade>= 60 and average_ <70:
+    print "your average grade is a D"
+if grade_average>=70 and average_ <80:
+    print "your average is a C"
+if grade_average>=80 and average_ <90:
+    print "your average is a B"
+if grade_average>=90 and average_ <100:
+    print "your average is a A"
+    
+
+   
+
+    
+ 
+ 
